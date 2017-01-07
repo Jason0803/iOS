4c4e2096c6efd5ea66a8cd7039bddfc42eddafab
Jason <c3171700@uon.edu.au>
2017-01-07 11:20:06 +0900

    1st

---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// variables
+var str:String = "Hello, playground"
+str = "Change"
+
+type(of: str)
+
+
+// constants
+let str2 = "Hello"
+
+
+print(str)
+print(str2)
+
+let integer_1 = 10
+type(of: integer_1)
+
+let integer_2:Int8 = 8
+type(of: integer_2)
+
+let double_1 = 3.1415926
+type(of: double_1)
+
+let float_1:Float = 2.71818182845904
+type(of:float_1)
+
+let bool_1 = true // false
+type(of: bool_1)
+
+let str_5 = "123ABC"
+let int_a = Int(str_5)
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Soo Young Choi on 1/7/17.
+//  Copyright © 2017 Jason Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
